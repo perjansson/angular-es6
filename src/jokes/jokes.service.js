49072c414e6f1cf7c9656aa3c63c04cedae60dfd
Per Jansson <per.r.jansson@gmail.com>
2016-01-06 14:58:34 +0100

    constant for backend url

--- src/jokes/jokes.service.js
+++ src/jokes/jokes.service.js
@@ -2,12 +2,14 @@ import Joke from './joke';
 
 class JokesService {
 
+  const jokeBackend = 'http://api.icndb.com/jokes/random';
+
   constructor($http) {
     this.$http = $http;
   }
 
   fetchJoke() {
-    return this.$http.get('http://api.icndb.com/jokes/random')
+    return this.$http.get(jokeBackend)
       .then(response => {
         return new Joke(response.data.value.joke);
       });
